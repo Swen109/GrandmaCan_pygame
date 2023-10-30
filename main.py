182eb12e6dccea86e41ed3efea423dbac59c7c02
Swen109 <issa.pikachu@gmail.com>
2023-10-31 00:16:22 +0800

    1031

--- main.py
+++ main.py
@@ -27,6 +27,15 @@ rock_imgs = []
 for i in range(7):
     rock_imgs.append(pygame.image.load(os.path.join("Img", f"rock{i}.png")).convert())
 
+font_name = pygame.font.match_font('arial')
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.centerx = x
+    text_rect.top = y
+    surf.blit(text_surface, text_rect)
+
 class Player(pygame.sprite.Sprite): #繼承內建的sprite類別，位置如括號內
     def __init__(self): #初始函式有固定的寫法
         pygame.sprite.Sprite.__init__(self) #sprite內建的初始函式
