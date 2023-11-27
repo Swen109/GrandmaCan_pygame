41f3c5e3c3022f1aa6d3b35f9e3af06ed10ebe87
Swen109 <119842646+Swen109@users.noreply.github.com>
2023-11-27 18:52:28 +0800

    11/27

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ HEIGHT = 600
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (225, 0, 0)
+GREEN = (0, 225, 0)
 
 FPS = 60
 
@@ -26,6 +27,24 @@ for i in range(7):
     rocks_img.append(pygame.image.load(os.path.join("img", f"rock{i}.png")).convert())
 for i in rocks_img:
     i.set_colorkey(BLACK)
+expl_anim = {}
+expl_anim['lg'] = []
+expl_anim['sm'] = []
+for i in range(9):
+    expl_img = pygame.image.load(os.path.join("img", f"expl{i}.png")).convert()
+    expl_img.set_colorkey(BLACK)
+    expl_anim['lg'].append(pygame.transform.scale(expl_img, (75, 75)))
+    expl_anim['sm'].append(pygame.transform.scale(expl_img, (25, 25)))
+
+
+shoot_sound = pygame.mixer.Sound(os.path.join("sound", "shoot.wav"))
+expl_sound = [
+    pygame.mixer.Sound(os.path.join("sound", "expl0.wav")),
+    pygame.mixer.Sound(os.path.join("sound", "expl1.wav"))
+    ]
+
+pygame.mixer.music.load(os.path.join("sound", "background.ogg"))
+pygame.mixer.music.set_volume(0.4)
 
 clock = pygame.time.Clock()
 
@@ -37,8 +56,24 @@ def new_rock():
 font_name = os.path.join("font.ttf")
 def draw_text(surf, text, size, x, y):
     font = pygame.font.Font(font_name, size)
-    
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.centerx = x
+    text_rect.top = y
+    surf.blit(text_surface, text_rect)
 
+def draw_health(surf, hp, x, y):
+    if hp < 0:
+        hp = 0
+    elif hp > 100:
+        hp = 100
+    BAR_LENGTH = 100
+    BAR_HEIGHT = 10
+    fill = (hp / 100) * BAR_LENGTH
+    outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pygame.Rect(x, y, fill, BAR_HEIGHT)
+    pygame.draw.rect(surf, WHITE, outline_rect)
+    pygame.draw.rect(surf, GREEN, fill_rect)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -49,6 +84,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 8
+        self.health = 100
 
     def update(self):
         key_pressed = pygame.key.get_pressed()
@@ -65,6 +101,7 @@ class Player(pygame.sprite.Sprite):
         bullet = Bullet(self.rect.center, self.rect.top)
         bullets.add(bullet)
         all_sprites.add(bullet)
+        shoot_sound.play()
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -109,6 +146,28 @@ class Rock(pygame.sprite.Sprite):
             self.kill()
             new_rock()
 
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, center, size):
+        pygame.sprite.Sprite.__init__(self)
+        self.size = size
+    #     self.image = expl_anim[self.size][0]
+    #     self.rect = self.image.get_rect()
+    #     self.radius = self.rect.width / 2
+    #     self.rect.right = random.randrange(0, WIDTH + self.rect.width)
+    #     self.rect.y = random.randrange(-180, -100)
+    #     self.speedx = random.randrange(-5, 5)
+    #     self.speedy = random.randrange(2, 10)
+    #     self.rot_degree = random.randrange(-3, 3)
+    #     self.total_degree = 0
+
+    # def update(self):
+    #     self.rotate()
+    #     self.rect.x += self.speedx
+    #     self.rect.y += self.speedy
+    #     if self.rect.right < 0 or self.rect.left > WIDTH or self.rect.top > HEIGHT:
+    #         self.kill()
+    #         new_rock()
+
 all_sprites = pygame.sprite.Group()
 rocks = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
@@ -120,6 +179,7 @@ for i in range(8):
 all_sprites.add(player)
 
 score = 0
+pygame.mixer.music.play(-1)
 
 #遊戲迴圈
 running = True
@@ -134,16 +194,21 @@ while running:
                 player.shoot()         
     #
     all_sprites.update()
-    hits = pygame.sprite.groupcollide(bullets, rocks, True, True)
+    hits = pygame.sprite.groupcollide(rocks, bullets, True, True)
     for hit in hits:
         new_rock()
-        score += hit.radius
+        score += int(hit.radius)
+        random.choice(expl_sound).play()
 
     hits = pygame.sprite.spritecollide(player, rocks, True, pygame.sprite.collide_circle)
-
+    for hit in hits:
+        player.health -= int(hit.radius) / 2
     #
     screen.blit(background_img, (0, 0))
     all_sprites.draw(screen)
+    draw_text(screen, str(score), 20, WIDTH /  2, 10)
+    draw_health(screen, player.health, 5, 10)
+
     pygame.display.update()
 
 pygame.quit()
